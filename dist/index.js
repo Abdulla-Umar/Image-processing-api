8ff9b738cd79ad8aea2bcee01f1cc5fd5f637843
Abdulla-Umar <81812150+Abdulla-Umar@users.noreply.github.com>
2021-12-27 08:47:12 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -6,12 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var imageRouter_js_1 = require("./routes/imageRouter.js");
 var app = (0, express_1.default)();
-var port = 6000;
 app.get("/", function (req, res) {
     res.send("hello");
 });
 app.use("/api", imageRouter_js_1.router);
-app.listen(port, function () {
-    console.log("server running on port: ".concat(port));
+app.listen(3000, function () {
+    console.log("server running on port: 3000");
 });
 exports.default = app;
